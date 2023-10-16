c3ba5e4ce4804543134434073e9b5d545c5c3c22
WladyslawK <vladkurr7890@gmail.com>
2023-10-16 23:19:27 +0200

    debuging

--- test.js
+++ test.js
@@ -2,8 +2,13 @@ import fs from 'node:fs'
 
 const readPjson = async () => {
     const pjsonPath = new URL('./package.json', import.meta.url).pathname
+    const file = 'package.json'
     fs.readFile(pjsonPath, (err, data) => {
-        console.log(data)
+        if(err){
+            console.log(err)
+        }else{
+            console.log(data)
+        }
     })
 }
 
